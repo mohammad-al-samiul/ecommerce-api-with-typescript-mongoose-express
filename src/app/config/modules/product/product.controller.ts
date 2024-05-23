badea6a49d1f88d526874c8680a1d2ece642829a
Mohammad Al Samiul <alsamiul123@gmail.com>
2024-05-23 10:59:31 +0600

    get all product and get single product api create completed

--- src/app/config/modules/product/product.controller.ts
+++ src/app/config/modules/product/product.controller.ts
@@ -15,6 +15,35 @@ const createProduct = async (req: Request, res: Response) => {
   }
 };
 
+const getAllProducts = async (req: Request, res: Response) => {
+  try {
+    const result = await ProductServices.getAllProducts();
+    res.status(200).json({
+      success: true,
+      message: "Products fetched successfully!",
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getProductById = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const result = await ProductServices.getProductById(id);
+    res.status(200).json({
+      success: true,
+      message: "Products fetched successfully!",
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 export const ProductControllers = {
   createProduct,
+  getAllProducts,
+  getProductById,
 };
--- src/app/config/modules/product/product.service.ts
+++ src/app/config/modules/product/product.service.ts
@@ -6,6 +6,18 @@ const createProduct = async (payload: TProduct) => {
   return result;
 };
 
+const getAllProducts = async () => {
+  const result = await Product.find({});
+  return result;
+};
+
+const getProductById = async (id: string) => {
+  const result = await Product.findById(id);
+  return result;
+};
+
 export const ProductServices = {
   createProduct,
+  getAllProducts,
+  getProductById,
 };
