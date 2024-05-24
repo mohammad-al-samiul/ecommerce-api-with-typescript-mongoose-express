9ee6ae69bd0835934c726ebbc0f489df14ea159c
Mohammad Al Samiul <alsamiul123@gmail.com>
2024-05-24 10:21:03 +0600

    get all order api created

--- src/app/config/modules/orders/order.controller.ts
+++ src/app/config/modules/orders/order.controller.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from "express";
 import { OrderServices } from "./order.service";
+import orderValidationSchema from "./order.validation";
 
 const createOrder = async (req: Request, res: Response) => {
   try {
     const orderData = req.body;
-    const result = await OrderServices.createOrder(orderData);
+    const zodParseData = orderValidationSchema.parse(orderData);
+    const result = await OrderServices.createOrder(zodParseData);
     res.status(201).json({
       success: true,
       message: "Order created successfully!",
